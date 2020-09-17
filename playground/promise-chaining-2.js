5fdc77fcfe7182adeffa51e5298ca3b8cc17b231
shan5742 <asamshan456@gmail.com>
2020-09-17 21:54:15 +0100

    refactoring to async await for endpoints

--- playground/promise-chaining-2.js
+++ playground/promise-chaining-2.js
@@ -1,13 +1,27 @@
 require("../src/db/mongoose");
 const Task = require("../src/models/task");
 
-Task.findByIdAndDelete("5f638204b67296cd78950faf")
-  .then((task) => {
-    console.log(task);
-    return Task.countDocuments({ completed: false });
-  })
-  .then((result) => {
-    console.log(result);
+// Task.findByIdAndDelete("5f638204b67296cd78950faf")
+//   .then((task) => {
+//     console.log(task);
+//     return Task.countDocuments({ completed: false });
+//   })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const deleteAndCount = async (id) => {
+  const taskToDelete = await Task.findByIdAndDelete(id);
+  const count = await Task.countDocuments({ completed: true });
+  return count;
+};
+
+deleteAndCount("5f6381085a9cc2cc626df0f1")
+  .then((count) => {
+    console.log(count);
   })
   .catch((e) => {
     console.log(e);
