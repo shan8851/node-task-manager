b68bedc2865158bfb6fb12a38af948b66ba8c1b2
shan5742 <asamshan456@gmail.com>
2020-09-17 14:52:01 +0100

    adding validation and sanitisation for documents with mongoose and validator

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -10,15 +11,45 @@ const User = mongoose.model("User", {
   name: {
     type: String,
     required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    },
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error("Password must not include password");
+      }
+    },
   },
   age: {
     type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a positive number");
+      }
+    },
   },
 });
 
 // const me = new User({
-//   name: "Asam",
-//   age: 33,
+//   name: "   Asam Shan   ",
+//   email: "ASAMSHAN456@gmail.com    ",
+//   password: "hellohello",
 // });
 
 // me.save()
@@ -32,21 +63,23 @@ const User = mongoose.model("User", {
 const Task = mongoose.model("Task", {
   description: {
     type: String,
+    required: true,
+    trim: true,
   },
   completed: {
     type: Boolean,
+    default: false,
   },
 });
 
-const blog = new Task({
-  description: "Write new blog post",
-  completed: false,
+const task = new Task({
+  description: "Write new blog post                 ",
 });
 
-blog
+task
   .save()
   .then(() => {
-    console.log(blog);
+    console.log(task);
   })
   .catch((error) => {
     console.log("Error:", error);
