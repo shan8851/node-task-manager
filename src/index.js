c12a319765eb7696ea5dcd35dccf4c89dcf576d5
shan5742 <asamshan456@gmail.com>
2020-09-17 16:34:19 +0100

    adding status codes to success create methods

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.post("/users", (req, res) => {
   user
     .save()
     .then(() => {
-      res.send(user);
+      res.status(201).send(user);
     })
     .catch((error) => {
       res.status(400).send(error);
@@ -25,7 +25,7 @@ app.post("/tasks", (req, res) => {
   task
     .save()
     .then(() => {
-      res.send(task);
+      res.status(201).send(task);
     })
     .catch((error) => {
       res.status(400).send(error);
