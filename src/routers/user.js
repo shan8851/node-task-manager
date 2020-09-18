6ce8182bfbdbcdeee7f9fbc0be23986c68d69007
shan5742 <asamshan456@gmail.com>
2020-09-18 14:15:12 +0100

    adding ability to add and remove profile pic to user object

--- src/routers/user.js
+++ src/routers/user.js
@@ -98,7 +98,6 @@ router.delete("/users/me", auth, async (req, res) => {
 // Upload profile picture
 
 const upload = multer({
-  dest: "avatars",
   limits: {
     fileSize: 1000000,
   },
@@ -111,13 +110,24 @@ const upload = multer({
 });
 router.post(
   "/users/me/avatar",
+  auth,
   upload.single("avatar"),
-  (req, res) => {
+  async (req, res) => {
+    req.user.avatar = req.file.buffer;
+    await req.user.save();
     res.send();
   },
   (error, req, res, next) => {
     res.status(400).send({ error: error.message });
   }
 );
+
+// Remove profile picture
+
+router.delete("/users/me/avatar", auth, async (req, res) => {
+  req.user.avatar = undefined;
+  await req.user.save();
+  res.send();
+});
 
 module.exports = router;
